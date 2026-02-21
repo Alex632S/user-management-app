89d9b591bde423ca7ff45c723c596a80bf825948
Александр Смотров <jobalex632@gmail.com>
2026-02-22 01:58:20 +0300

    Revert to 0ef6b3c

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,31 +11,10 @@ export default defineNuxtConfig({
     '@vee-validate/nuxt'
   ],
 
-  nitro: {
-    preset: 'netlify',
-    output: {
-      publicDir: 'dist',
-      serverDir: '.netlify/functions-internal'
-    }
+   nitro: {
+    preset: 'netlify'
   },
-
-  app: {
-    baseURL: '/',
-    buildAssetsDir: '/_nuxt',
-    cdnURL: ''
-  },
-
-  css: [
-    '@/app/assets/styles/main.scss'
-  ],
-
-  vite: {
-    build: {
-      cssCodeSplit: true,
-      sourcemap: process.env.NODE_ENV !== 'production'
-    }
-  },
-
+  
   compatibilityDate: '2025-02-22',
 
   typescript: {
@@ -60,18 +39,5 @@ export default defineNuxtConfig({
     '@': './'
   },
 
-  ssr: true,
-
-  plugins: [
-    '~/app/plugins/msw.client.ts'
-  ],
-
-  runtimeConfig: {
-    public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api',
-      mockEnabled: process.env.NUXT_PUBLIC_MOCK_ENABLED === 'true',
-      environment: process.env.NODE_ENV || 'development'
-    },
-    apiSecret: process.env.NUXT_API_SECRET
-  }
+  ssr: true
 })
