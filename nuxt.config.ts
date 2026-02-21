dbe39343ee8f6d418ed238035d4eb828943fc4cb
Александр Смотров <jobalex632@gmail.com>
2026-02-22 00:08:20 +0300

    chore: update package-lock.json]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,10 +11,31 @@ export default defineNuxtConfig({
     '@vee-validate/nuxt'
   ],
 
-   nitro: {
-    preset: 'netlify'
+  nitro: {
+    preset: 'netlify',
+    output: {
+      publicDir: 'dist',
+      serverDir: '.netlify/functions-internal'
+    }
   },
-  
+
+  app: {
+    baseURL: '/',
+    buildAssetsDir: '/_nuxt',
+    cdnURL: ''
+  },
+
+  css: [
+    '~/assets/styles/main.scss'
+  ],
+
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      sourcemap: process.env.NODE_ENV !== 'production'
+    }
+  },
+
   compatibilityDate: '2025-02-22',
 
   typescript: {
@@ -39,5 +60,18 @@ export default defineNuxtConfig({
     '@': './'
   },
 
-  ssr: true
+  ssr: true,
+
+  plugins: [
+    '~/app/plugins/msw.client.ts'
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api',
+      mockEnabled: process.env.NUXT_PUBLIC_MOCK_ENABLED === 'true',
+      environment: process.env.NODE_ENV || 'development'
+    },
+    apiSecret: process.env.NUXT_API_SECRET
+  }
 })
