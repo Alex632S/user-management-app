e52490545456b13a6f22456d884c8f7cdc62457e
Александр Смотров <jobalex632@gmail.com>
2026-03-30 18:20:38 +0300

    refactor: иконки

--- app/pages/kit.vue
+++ app/pages/kit.vue
@@ -10,7 +10,23 @@
       <Badge variant="default">Default</Badge>
     </div>
     <div class="m-3 flex space-x-2">
-      <Icon name="plus" color="blue-600" size="6" />
+      <Icon name="plus" color="blue-600" size="4" />
+      <Icon name="dashboard" color="blue-600" size="5" />
+      <Icon name="team" color="blue-600" size="6" />
+      <Icon name="projects" color="blue-600" size="8" />
+      <Icon name="calendar" color="blue-600" size="10" />
+      <Icon name="documents" color="blue-600" size="12" />
+      <Icon name="settings" color="blue-600" size="12" />
+      <Icon name="chevron-down" color="blue-600" size="4" />
+      <Icon name="chevron-up" color="blue-600" size="5" />
+      <Icon name="search" color="blue-600" size="6" />
+      <Icon name="menu" color="blue-600" size="8" />
+      <Icon name="check" color="blue-600" size="10" />
+      <Icon name="sort-asc" color="blue-600" size="12" />
+      <Icon name="sort-desc" color="blue-600" size="12" />
+      <Icon name="sort-desc" color="blue-600" size="5" />
+      <Icon name="arrow-up" color="blue-600" size="6" />
+      <Icon name="arrow-down" color="blue-600" size="8" />
     </div>
   </div>
 </template>
